14e55a3d21128e066ba09981d8ede8d19f28f03b
Ilman Maulana <ilman.maulana@gmail.com>
2015-02-09 19:28:07 +0700

    Add new field to log

--- src/Teepluss/Monosql/MySQLHandler.php
+++ src/Teepluss/Monosql/MySQLHandler.php
@@ -4,6 +4,11 @@ use Monolog\Logger;
 use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\AbstractProcessingHandler;
 
+use Scienceguard\SG_Util;
+use Request;
+use Input;
+use Session;
+
 class MySQLHandler extends AbstractProcessingHandler {
 
     protected $connection;
@@ -33,7 +38,14 @@ class MySQLHandler extends AbstractProcessingHandler {
             'level_name' => $record['level_name'],
             'context'    => json_encode($record['context']),
             'formatted'  => $record['formatted'],
-            'created_at' => $record['datetime']->format('Y-m-d H:i:s')
+            'created_at' => $record['datetime']->format('Y-m-d H:i:s'),
+
+            'url'           => Request::url(),
+            'method'        => Request::method(),
+            'ref_url'       => SG_Util::val($_SERVER, 'HTTP_REFERER', ''),
+            'ip_address'    => SG_Util::val($_SERVER, 'REMOTE_ADDR', ''),
+            'input'         => json_encode(Input::all()),
+            'session'       => json_encode(Session::all()),
         );
 
         $this->connection->table($this->table)->insert($data);
--- src/migrations/2013_11_28_045114_create_logs_table.php
+++ src/migrations/2013_11_28_045114_create_logs_table.php
@@ -22,14 +22,20 @@ class CreateLogsTable extends Migration {
 		{
 			$table->engine = 'InnoDB';
 
-		    $table->bigIncrements('id');
-		    $table->string('channel', 50)->index();
-		    $table->text('message');
-		    $table->string('level', 50)->index();
-		    $table->string('level_name', 100);
-		    $table->text('context');
-		    $table->text('formatted');
-		    $table->dateTime('created_at');
+			$table->bigIncrements('id');
+			$table->string('channel', 50)->index();
+			$table->string('level', 50)->index();
+			$table->string('level_name', 100);
+			$table->text('url');
+			$table->string('method', 10);
+			$table->text('message');
+			$table->string('ip_address', 50);
+			$table->text('ref_url');
+			$table->text('session');
+			$table->text('input');
+			$table->text('context');
+			$table->text('formatted');
+			$table->dateTime('created_at');
 		});
 	}
 
